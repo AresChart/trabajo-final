0bf227978e19dfe18840a8ff17cb65045b4b2752
Kevin David Sanchez Solis <kevindavidsanchezsolis@gmail.com>
2022-03-14 21:05:46 -0500

    Organiza segmentacion y asignacion de disco

--- scripts_am/Main.js
+++ scripts_am/Main.js
@@ -28,7 +28,7 @@ export let mapaBits = inicializarMapa();
         //Arreglo que almacena el tamaño de caracteres de los archivos creados
         var tamañoCaracteresEnlazada = [];
         //Arreglo con los indices de inicio de cada archivo
-        var inicioEnlazada = [];
+        export var inicioEnlazada = [];
         //Arreglo que funciona como mapa de bits
         export var mapaEnlazada = crearMapa();
         // Log para disco de memoria Enlazada
@@ -39,7 +39,7 @@ export let mapaBits = inicializarMapa();
         //Arreglo que almacena el tamaño de caracteres de los archivos creados
         var tamañoCaracteresIndexadaEnlazada = [];
         //Arreglo con los indices de inicio de cada archivo
-        var inicioIndexadaEnlazada = [];
+        export var inicioIndexadaEnlazada = [];
         //Arreglo con las posiciones ocupadas por cada archivo
         var posicionesIndexadaEnlazada = [];
         //Arreglo que funciona como mapa de bits
@@ -52,7 +52,7 @@ export let mapaBits = inicializarMapa();
         //Arreglo que almacena el tamaño de caracteres de los archivos creados
         var tamañoCaracteresIndexadaMultinivel = [];
         //Arreglo con los indices de inicio de cada archivo
-        var inicioIndexadaMultinivel = [];
+        export var inicioIndexadaMultinivel = [];
         //Arreglo con las posiciones ocupadas por cada archivo
         var posicionesIndexadaMultinivel = [];
         //Arreglo que funciona como mapa de bits
@@ -65,7 +65,7 @@ export let mapaBits = inicializarMapa();
         //Arreglo que almacena el tamaño de caracteres de los archivos creados
         var tamañoCaracteresIndexadaCombinada  = [];
         //Arreglo con los indices de inicio de cada archivo
-        var inicioIndexadaCombinada  = [];
+        export var inicioIndexadaCombinada  = [];
         //Arreglo con las posiciones ocupadas por cada archivo
         var posicionesIndexadaCombinada  = [];
         //Arreglo que funciona como mapa de bits
